6596f21fe29c21fc20bf91b97e2bc5b64f696508
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2023-10-18 16:30:54 +0600

    For keeping and  using brands image and name in database, post get and delete have been used

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,  ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,10 +21,50 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
+
+  const brandsCollection = client.db("brandsDB").collection("brands");
+
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post('/brands', async(req, res) =>{
+      const brand = req.body;
+      const result = await brandsCollection.insertOne(brand);
+      res.send(result);
+    })
+
+    app.get('/brands', async(req, res) =>{
+      const cursor = brandsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/brands/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const brand = await brandsCollection.findOne(query);
+      res.send(brand);
+   })
+
+    app.delete('/brands/:id', async(req, res) =>{
+      const id = req.params.id;
+      console.log("delete this", id);
+      const query = {_id: new ObjectId(id) };
+      const result = await brandsCollection.deleteOne(query);
+      res.send(result);
+
+    })
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
